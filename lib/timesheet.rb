3c75c7c5fa3979edb4e99c4f80675aa94d41e25c
Sabrina Robinson <sabrinarob@gmail.com>
2018-02-27 11:01:37 -0700

    Adding the timesheet module to the timesheet class.

--- lib/timesheet.rb
+++ lib/timesheet.rb
@@ -1,13 +1,19 @@
+require './modules/date_handler'
+
 class Timesheet
   attr_reader :employee_id,
               :project_id,
-              :start_date,
+              # :start_date,
               :minutes
   def initialize(arguments)
-    @employee_id = arguments[:employee_id]
-    @project_id  = arguments[:project_id]
-    @start_date  = arguments[:start_date]
-    @minutes     = arguments[:minutes]
+    @employee_id = arguments[:employee_id].to_i
+    @project_id  = arguments[:project_id].to_i
+    # @start_date  = arguments[:start_date]
+    @minutes     = arguments[:minutes].to_i
   end
 
+  def start_datestring_to_date
+    require date_handler
+    string_to_date
+  end
 end
--- test/timesheet_test.rb
+++ test/timesheet_test.rb
@@ -10,11 +10,11 @@ class TimesheetTest < Minitest::Test
   end
 
   def test_attributes
-    skip
+
     timesheet = Timesheet.new({employee_id: '5', project_id: '7', date:'2015-01-01', minutes: '120'})
-    assert_equal 7, timesheet.employee_id
-    assert_equal 5, timesheet.project_id
-    assert_equal 2015-01-01, timesheet.date
+    assert_equal 5, timesheet.employee_id
+    assert_equal 7, timesheet.project_id
+    assert_equal 2015-01-01, timesheet.start_date
     assert_equal 120, timesheet.minutes
   end
 end
