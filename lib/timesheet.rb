ee2114efe86ba81de55743d9485568b932886340
Sabrina Robinson <sabrinarob@gmail.com>
2018-02-27 11:50:31 -0700

    Adjusting test for timesheet.

--- lib/timesheet.rb
+++ lib/timesheet.rb
@@ -1,23 +1,12 @@
-require './modules/date_handler'
-
 class Timesheet
   attr_reader :employee_id,
               :project_id,
-              # :start_date,
+              :start_date,
               :minutes
   def initialize(arguments)
     @employee_id = arguments[:employee_id].to_i
     @project_id  = arguments[:project_id].to_i
-    # @start_date  = arguments[:start_date]
+    @start_date  = arguments[:start_date]
     @minutes     = arguments[:minutes].to_i
-  end
-
-  def start_date(start_date)
-    date_bits = date_string.split('-')
-    start_date = (
-      date_bits[0].to_i,
-      date_bits[1].to_i,
-      date_bits[2].to_i
-    )
   end
 end
--- test/timesheet_test.rb
+++ test/timesheet_test.rb
@@ -5,13 +5,13 @@ require './lib/timesheet'
 
 class TimesheetTest < Minitest::Test
   def test_instantiation
-    timesheet = Timesheet.new({employee_id: '5', project_id: '7', date:'2015-01-01', minutes: '120'})
+    timesheet = Timesheet.new({employee_id: '5', project_id: '7', date: 2015-01-01, minutes: 120})
     assert_instance_of Timesheet, timesheet
   end
 
   def test_attributes
 
-    timesheet = Timesheet.new({employee_id: '5', project_id: '7', date:'2015-01-01', minutes: '120'})
+    timesheet = Timesheet.new({employee_id: '5', project_id: '7', date: 2015-01-01, minutes: 120})
     assert_equal 5, timesheet.employee_id
     assert_equal 7, timesheet.project_id
     assert_equal 2015-01-01, timesheet.start_date
