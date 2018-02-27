60ce18c70c2b66a885e97ef4a3f34d8dfa40d8ff
Sabrina Robinson <sabrinarob@gmail.com>
2018-02-27 10:32:34 -0700

    Updating project test to test attributes.

--- test/project_test.rb
+++ test/project_test.rb
@@ -7,4 +7,12 @@ class ProjectTest < Minitest::Test
     project = Project.new({project_id: '123', name: 'Widget Maker', start_date: 2015-01-01, end_date: 2018-01-01})
     assert_instance_of Project, project
   end
+
+  def test_attributes
+    project = Project.new({project_id: '123', name: 'Widget Maker', start_date: 2015-01-01, end_date: 2018-01-01})
+    assert_equal 123, project.project_id
+    assert_equal 'Widget Maker', project.name
+    assert_equal 2015-01-01, project.start_date
+    assert_equal 2018-01-01, project.end_date
+  end
 end
