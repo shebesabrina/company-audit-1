7ed1445a7f8ee9468bfe770c6b66baeaa2eb5d0a
Sabrina Robinson <sabrinarob@gmail.com>
2018-02-27 09:40:35 -0700

    Initial commit.

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,15 @@
 class Employee
+  attr_reader :employee_id,
+              :name,
+              :role,
+              :start_date,
+              :end_date
+  def initialize(arguments)
+    @employee_id = arguments[:employee_id]
+    @name        = arguments[:name]
+    @role        = arguments[:role]
+    @start_date  = arguments[:start_date]
+    @end_date    = arguments[:end_date]
+  end
+
 end
--- test/employee_test.rb
+++ test/employee_test.rb
@@ -3,6 +3,8 @@ require './lib/employee'
 
 class EmployeeTest < Minitest::Test
   def test_instantiation
+    employee = Employee.new({employee_id: '5', name: 'Sally Jackson', role: 'Engineer', start_date: '2015-01-01', end_date: '2018-01-01'})
+    assert_instance_of Employee, employee
   end
 
   def test_attributes
